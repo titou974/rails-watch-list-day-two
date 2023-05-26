dfb6fc3f3a6249f5d20ed4a2b9dea8b204949e74
Titouan Hirsch <titouanhirsch@gmail.com>
2023-05-26 17:13:31 +0200

    cloudinary almost done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.active_storage.service = :cloudinary
+
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
